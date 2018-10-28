c28726bc8adcf0538f56aa22ce4fbb9a24b60d89
Daniela Cortes <danielacortes@Danielas-MacBook-Air.local>
2018-10-28 13:32:32 -0300

    Add:temporary session buttons

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -49,7 +49,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
-
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
   #   super(resource)
