275d8e1c2bd66b054a3ad54f171d10b71c46802e
Daniela Cortes <danielacortes@Danielas-MacBook-Air.local>
2018-10-28 14:09:06 -0300

    ADD:Tasks and Users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+9.times do |i|
+  Task.create(
+    name: "Tarea #{i + 1}",
+    photo: "https://loremflickr.com/g/320/240/chile"
+  )
+end
+
+10.times do |x|
+  User.create!(
+    email: "email#{x+1}@email.com",
+    password: "password",
+    password_confirmation: "password",
+    name: Faker::HarryPotter.character,
+    photo: "https://loremflickr.com/250/250/woman"
+  )
+end
