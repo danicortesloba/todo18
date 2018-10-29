2c77dd65834b079b6d86affed7a37470ed6ef88e
Daniela Cortes <danielacortes@Danielas-MacBook-Air.local>
2018-10-28 22:10:54 -0300

    ADD: temporary complete and incomplete buttons

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,30 @@
 class TasksController < ApplicationController
+before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
+  end
+
+  def complete
+  @task = Task.find(params[:id])
+  @task.users << current_user
+  @task.save
+  redirect_to tasks_path
+end
+
+  def incomplete
+    user = current_user
+    @task = Task.find(params[:id])
+    @task.users.delete(user)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def task_params
+    params.require(:task).permit(:name, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks
+  get 'tasks/:id/complete', to: 'tasks#complete', as:'tasks_complete'
+  delete 'tasks/:id/incomplete', to: 'tasks#incomplete', as:'tasks_incomplete'
   devise_for :users, controllers: {registrations: 'users/registrations'}
   root to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
