49183683d920d3695e09a0a0d9df51d3717d5b5e
Daniela Cortes <danielacortes@Danielas-MacBook-Air.local>
2018-10-28 21:31:46 -0300

    Add:JoinTable

---
+++ db/migrate/20181029002055_create_join_table.rb
@@ -0,0 +1,9 @@
+class CreateJoinTable < ActiveRecord::Migration[5.2]
+  def change
+
+
+      # t.index [:user_id, :task_id]
+      # t.index [:task_id, :user_id]
+  
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_28_174440) do
+ActiveRecord::Schema.define(version: 2018_10_29_002850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,11 @@ ActiveRecord::Schema.define(version: 2018_10_28_174440) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "done"
+  end
+
+  create_table "tasks_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "task_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
